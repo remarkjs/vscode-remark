cc44890b6e899fcdbe67add092114e4740b4f587
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2020-06-28 01:13:45 +0200

    Test the config completely

--- src/test/suite/workspace.test.ts
+++ src/test/suite/workspace.test.ts
@@ -6,6 +6,7 @@ import { after } from 'mocha';
 import { before } from 'mocha';
 import * as chai from 'chai';
 import * as path from 'path';
+import * as check from 'check-types';
 
 const assert = chai.assert;
 const expect = chai.expect;
@@ -29,5 +30,22 @@ suite('Extension Test Suite', () => {
 		const config = await workspace.parseWorkspaceConfig(files);
 
 		expect(config, 'config').to.not.be.null;
+		expect(config.plugins, 'config.plugins').to.not.be.null;
+
+		expect(check.array(config.plugins), 'config.plugins').to.be.true;
+		expect(config.plugins, 'config.plugins').to.have.lengthOf(2);
+		expect(config.plugins[0], 'config.plugins[0]').to.eq('first-heading');
+		expect(config.plugins[1], 'config.plugins[1]').to.eq('github');
+
+		expect(check.object(config.settings), 'config.settings').to.be.true;
+		expect(config.plugins.closeAtx, 'config.settings.closeAtx').not.to.be.true;
+
+		expect(check.object(config.github), 'config.github').to.be.true;
+		expect(config.github.repository, 'config.github.repository').to.be.eq('https://github.com/mrmlnc/vscode-remark');
+
+		console.log(JSON.stringify(config));
+
+		expect(check.object(config['first-heading']), 'config["first-heading"]').to.be.true;
+		expect(config['first-heading'].heading, 'config["first-heading"].heading').to.be.eq('Hello, .remarkrc file!');
 	});
 });
