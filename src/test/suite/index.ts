c6cf129c6822ff3257420c710273c03378beb4ce
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2021-03-22 12:17:00 +0100

    Add coverage checks
    Closes GH-34.
    
    Reviewed-by: Christian Murphy <christian.murphy.42@gmail.com>
    Reviewed-by: Titus Wormer <tituswormer@gmail.com>

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -1,38 +1,50 @@
+'use strict';
+
 import * as path from 'path';
 import * as Mocha from 'mocha';
 import * as glob from 'glob';
 
-export function run(): Promise<void> {
-	// Create the mocha test
+// The test coverage approach is inspired by https://github.com/microsoft/vscode-js-debug/blob/master/src/test/testRunner.ts
+function setupCoverage() {
+    const NYC = require('nyc');
+	const nyc = new NYC({
+		cwd: path.join(__dirname, '..', '..', '..'),
+		exclude: ['**/test/**', '.vscode-test/**'],
+		reporter: ['lcov'],
+		all: true,
+		instrument: true,
+		hookRequire: true,
+		hookRunInContext: true,
+		hookRunInThisContext: true,
+	});
+
+    nyc.reset();
+    nyc.wrap();
+
+    return nyc;
+}
+
+export async function run(): Promise<void> {
+	const nyc = process.env.COVERAGE ? setupCoverage() : null;
 	const mocha = new Mocha({
 		ui: 'tdd',
-		color: true
+		color: true,
+		timeout: 10 * 1000
 	});
-
 	const testsRoot = path.resolve(__dirname, '..');
+	const files = glob.sync('**/*.test.js', { cwd: testsRoot });
+	files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
 
-	return new Promise((c, e) => {
-		glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
-			if (err) {
-				return e(err);
-			}
-
-			// Add files to the test suite
-			files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
-
-			try {
-				// Run the mocha test
-				mocha.run(failures => {
-					if (failures > 0) {
-						e(new Error(`${failures} tests failed.`));
-					} else {
-						c();
-					}
-				});
-			} catch (err) {
-				console.error(err);
-				e(err);
-			}
-		});
-	});
+	try {
+		await new Promise((resolve, reject) =>
+			mocha.run(failures =>
+				failures ? reject(new Error(`${failures} tests failed`)) : resolve(undefined),
+			)
+		);
+	} finally {
+		if (nyc) {
+			nyc.writeCoverageFile();
+			await nyc.report();
+		}
+	}
 }
