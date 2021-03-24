c328faf58c3b163ec07406d960e23e0613780a07
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2021-03-24 12:02:23 +0100

    Indent with 4 spaces
    - Change the .editorconfig setting to indent with spaces instead of tabs.
    - Format all files according to the indent setting.

--- src/utils/workspace.ts
+++ src/utils/workspace.ts
@@ -5,38 +5,38 @@ import { promises as fs } from 'fs';
 import * as yaml from 'yaml';
 
 export async function getWorkspaceConfig() {
-	const files = await vscode.workspace.findFiles('**/*remarkrc*', '**/node_modules/**');
-	return await parseWorkspaceConfig(files);
+    const files = await vscode.workspace.findFiles('**/*remarkrc*', '**/node_modules/**');
+    return await parseWorkspaceConfig(files);
 }
 
-export async function parseWorkspaceConfig(files : vscode.Uri[]) {
-	if (files.length === 0) {
-		return null;
-	}
+export async function parseWorkspaceConfig(files: vscode.Uri[]) {
+    if (files.length === 0) {
+        return null;
+    }
 
-	const fileName = files[0].fsPath;
+    const fileName = files[0].fsPath;
 
-	if (fileName.endsWith('.js')) {
-		try {
-			return require(files[0].fsPath);
-		}
-		catch (err) {
-			console.error(err);
-			return 'SyntaxError';
-		}
-	}
+    if (fileName.endsWith('.js')) {
+        try {
+            return require(files[0].fsPath);
+        }
+        catch (err) {
+            console.error(err);
+            return 'SyntaxError';
+        }
+    }
 
-	const content = await fs.readFile(files[0].fsPath);
-	
-	try {
-		if (fileName.endsWith('.yml') || fileName.endsWith('.yaml')) {
-			return yaml.parse(content.toString());
-		}
-		
-		return JSON.parse(content.toString());
-	}
-	catch (err) {
-		console.error(err);
-		return 'SyntaxError';
-	}
+    const content = await fs.readFile(files[0].fsPath);
+
+    try {
+        if (fileName.endsWith('.yml') || fileName.endsWith('.yaml')) {
+            return yaml.parse(content.toString());
+        }
+
+        return JSON.parse(content.toString());
+    }
+    catch (err) {
+        console.error(err);
+        return 'SyntaxError';
+    }
 }
