d7e561287e69e8393d0fc791b5a94a81dcc337f4
Remco Haszing <remcohaszing@gmail.com>
2021-12-23 10:54:33 +0100

    Use `prettier`
    Closes GH-81.
    
    Reviewed-by: Christian Murphy <christian.murphy.42@gmail.com>
    Reviewed-by: Titus Wormer <tituswormer@gmail.com>

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -1,50 +1,52 @@
-'use strict';
+'use strict'
 
-import * as path from 'path';
-import * as Mocha from 'mocha';
-import * as glob from 'glob';
+import * as path from 'path'
+import * as Mocha from 'mocha'
+import * as glob from 'glob'
 
 // The test coverage approach is inspired by https://github.com/microsoft/vscode-js-debug/blob/master/src/test/testRunner.ts
 function setupCoverage() {
-    const NYC = require('nyc');
-    const nyc = new NYC({
-        cwd: path.join(__dirname, '..', '..', '..'),
-        exclude: ['**/test/**', '.vscode-test/**'],
-        reporter: ['lcov'],
-        all: true,
-        instrument: true,
-        hookRequire: true,
-        hookRunInContext: true,
-        hookRunInThisContext: true,
-    });
+  const NYC = require('nyc')
+  const nyc = new NYC({
+    cwd: path.join(__dirname, '..', '..', '..'),
+    exclude: ['**/test/**', '.vscode-test/**'],
+    reporter: ['lcov'],
+    all: true,
+    instrument: true,
+    hookRequire: true,
+    hookRunInContext: true,
+    hookRunInThisContext: true
+  })
 
-    nyc.reset();
-    nyc.wrap();
+  nyc.reset()
+  nyc.wrap()
 
-    return nyc;
+  return nyc
 }
 
 export async function run(): Promise<void> {
-    const nyc = process.env.COVERAGE ? setupCoverage() : null;
-    const mocha = new Mocha({
-        ui: 'tdd',
-        color: true,
-        timeout: 10 * 1000
-    });
-    const testsRoot = path.resolve(__dirname, '..');
-    const files = glob.sync('**/*.test.js', { cwd: testsRoot });
-    files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
+  const nyc = process.env.COVERAGE ? setupCoverage() : null
+  const mocha = new Mocha({
+    ui: 'tdd',
+    color: true,
+    timeout: 10 * 1000
+  })
+  const testsRoot = path.resolve(__dirname, '..')
+  const files = glob.sync('**/*.test.js', {cwd: testsRoot})
+  files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)))
 
-    try {
-        await new Promise((resolve, reject) =>
-            mocha.run(failures =>
-                failures ? reject(new Error(`${failures} tests failed`)) : resolve(undefined),
-            )
-        );
-    } finally {
-        if (nyc) {
-            nyc.writeCoverageFile();
-            await nyc.report();
-        }
+  try {
+    await new Promise((resolve, reject) =>
+      mocha.run((failures) =>
+        failures
+          ? reject(new Error(`${failures} tests failed`))
+          : resolve(undefined)
+      )
+    )
+  } finally {
+    if (nyc) {
+      nyc.writeCoverageFile()
+      await nyc.report()
     }
+  }
 }
--- src/utils/workspace.ts
+++ src/utils/workspace.ts
@@ -1,42 +1,43 @@
-'use strict';
+'use strict'
 
-import * as vscode from 'vscode';
-import { promises as fs } from 'fs';
-import * as yaml from 'yaml';
+import * as vscode from 'vscode'
+import {promises as fs} from 'fs'
+import * as yaml from 'yaml'
 
 export async function getWorkspaceConfig() {
-    const files = await vscode.workspace.findFiles('**/*remarkrc*', '**/node_modules/**');
-    return await parseWorkspaceConfig(files);
+  const files = await vscode.workspace.findFiles(
+    '**/*remarkrc*',
+    '**/node_modules/**'
+  )
+  return await parseWorkspaceConfig(files)
 }
 
 export async function parseWorkspaceConfig(files: vscode.Uri[]) {
-    if (files.length === 0) {
-        return null;
-    }
+  if (files.length === 0) {
+    return null
+  }
 
-    const fileName = files[0].fsPath;
+  const fileName = files[0].fsPath
 
-    if (fileName.endsWith('.js')) {
-        try {
-            return require(files[0].fsPath);
-        }
-        catch (err) {
-            console.error(err);
-            return 'SyntaxError';
-        }
-    }
-
-    const content = await fs.readFile(files[0].fsPath);
-
+  if (fileName.endsWith('.js')) {
     try {
-        if (fileName.endsWith('.yml') || fileName.endsWith('.yaml')) {
-            return yaml.parse(content.toString());
-        }
+      return require(files[0].fsPath)
+    } catch (err) {
+      console.error(err)
+      return 'SyntaxError'
+    }
+  }
 
-        return JSON.parse(content.toString());
+  const content = await fs.readFile(files[0].fsPath)
+
+  try {
+    if (fileName.endsWith('.yml') || fileName.endsWith('.yaml')) {
+      return yaml.parse(content.toString())
     }
-    catch (err) {
-        console.error(err);
-        return 'SyntaxError';
-    }
+
+    return JSON.parse(content.toString())
+  } catch (err) {
+    console.error(err)
+    return 'SyntaxError'
+  }
 }
