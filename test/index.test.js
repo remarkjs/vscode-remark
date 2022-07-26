8ae69ce46ca593ca6d09963de734db0a09e83471
Remco Haszing <remcohaszing@gmail.com>
2022-07-26 22:18:55 +0200

    Fix flaky test
    The promise returned by `fs.writeFile` wasn’t awaited.
    
    Also `workspace.openTectDocument` accepts a string URI,

--- test/index.test.js
+++ test/index.test.js
@@ -2,7 +2,7 @@ const assert = require('node:assert')
 const fs = require('node:fs/promises')
 const path = require('node:path')
 
-const {commands, extensions, Uri, window, workspace} = require('vscode')
+const {commands, extensions, window, workspace} = require('vscode')
 
 module.exports.run = async () => {
   const filePath = path.join(__dirname, 'test.md')
@@ -10,9 +10,8 @@ module.exports.run = async () => {
     const ext = extensions.getExtension('unifiedjs.vscode-remark')
     await ext?.activate()
 
-    fs.writeFile(filePath, '- remark\n- lsp\n- vscode\n')
-    const uri = Uri.file(filePath)
-    const doc = await workspace.openTextDocument(uri)
+    await fs.writeFile(filePath, '- remark\n- lsp\n- vscode\n')
+    const doc = await workspace.openTextDocument(filePath)
     await window.showTextDocument(doc)
     await commands.executeCommand('editor.action.formatDocument')
 
