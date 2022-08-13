6bf4020bf0be486153ad00fe03efe666f0b0128b
Remco Haszing <remcohaszing@gmail.com>
2022-08-13 14:29:27 +0200

    Rename index.test.js to index.test.cjs
    This makes XO not complain about CommonJS syntax by default.

--- test.mjs
+++ test.mjs
@@ -3,5 +3,5 @@ import {runTests} from '@vscode/test-electron'
 
 await runTests({
   extensionDevelopmentPath: new URL('.', import.meta.url).pathname,
-  extensionTestsPath: new URL('test/index.test.js', import.meta.url).pathname
+  extensionTestsPath: new URL('test/index.test.cjs', import.meta.url).pathname
 })
