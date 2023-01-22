c8ffa1591bc7057d8d0f08d84769d2cca94a5b1a
Remco Haszing <remcohaszing@gmail.com>
2023-01-22 13:00:58 +0100

    Simplify debugging configuration

--- src/extension.js
+++ src/extension.js
@@ -1,5 +1,5 @@
 import {workspace} from 'vscode'
-import {LanguageClient, TransportKind} from 'vscode-languageclient/node.js'
+import {LanguageClient} from 'vscode-languageclient/node.js'
 
 /**
  * @type {LanguageClient}
@@ -10,38 +10,21 @@ let client
  * @param {import('vscode').ExtensionContext} context
  */
 export async function activate(context) {
-  /**
-   * @type {import('vscode-languageclient/node').NodeModule}
-   */
-  const run = {
-    module: context.asAbsolutePath('out/remark-language-server.js'),
-    transport: TransportKind.ipc
-  }
-
-  /**
-   * @type {import('vscode-languageclient/node').ServerOptions}
-   */
-  const serverOptions = {
-    run,
-    debug: {...run, options: {execArgv: ['--inspect=6009']}}
-  }
-
-  /**
-   * @type {import('vscode-languageclient').LanguageClientOptions}
-   */
-  const clientOptions = {
-    documentSelector: [{scheme: 'file', language: 'markdown'}],
-    synchronize: {
-      fileEvents: [
-        workspace.createFileSystemWatcher(
-          '**/.remark{ignore,rc,rc.cjs,rc.js,rc.json,rc.mjs,rc.yaml,rc.yml}'
-        ),
-        workspace.createFileSystemWatcher('**/package.json')
-      ]
+  client = new LanguageClient(
+    'remark',
+    {module: context.asAbsolutePath('out/remark-language-server.js')},
+    {
+      documentSelector: [{scheme: 'file', language: 'markdown'}],
+      synchronize: {
+        fileEvents: [
+          workspace.createFileSystemWatcher(
+            '**/.remark{ignore,rc,rc.cjs,rc.js,rc.json,rc.mjs,rc.yaml,rc.yml}'
+          ),
+          workspace.createFileSystemWatcher('**/package.json')
+        ]
+      }
     }
-  }
-
-  client = new LanguageClient('remark', serverOptions, clientOptions)
+  )
 
   await client.start()
 }
