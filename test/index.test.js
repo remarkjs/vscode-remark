d5cf1b80b5bf5282da51017e3be7b21bbdd800ae
Remco Haszing <remcohaszing@gmail.com>
2023-08-22 17:34:14 +0200

    Update dependencies (#128)
    The test was rewritten to use `node:test`.

--- test/index.test.js
+++ test/index.test.js
@@ -1,28 +1,23 @@
-const assert = require('node:assert')
+const assert = require('node:assert/strict')
 const fs = require('node:fs/promises')
 const path = require('node:path')
-
+const {test} = require('node:test')
 const {commands, extensions, window, workspace} = require('vscode')
 
 module.exports.run = async () => {
   const filePath = path.join(__dirname, 'test.md')
-  try {
-    const ext = extensions.getExtension('unifiedjs.vscode-remark')
-    await ext?.activate()
 
+  const ext = extensions.getExtension('unifiedjs.vscode-remark')
+  await ext?.activate()
+
+  await test('use the language server', async () => {
     await fs.writeFile(filePath, '- remark\n- lsp\n- vscode\n')
     const doc = await workspace.openTextDocument(filePath)
     await window.showTextDocument(doc)
     await commands.executeCommand('editor.action.formatDocument')
 
-    assert.equal(
-      doc.getText(),
-      '*   remark\n*   lsp\n*   vscode\n',
-      'format document'
-    )
-  } catch (error) {
-    throw error
-  } finally {
-    await fs.rm(filePath)
-  }
+    assert.equal(doc.getText(), '*   remark\n*   lsp\n*   vscode\n')
+  })
+
+  await fs.rm(filePath, {force: true})
 }
