d8a0341029461913b6cfb60d859b24217cb652e6
Remco Haszing <remcohaszing@gmail.com>
2026-02-14 13:31:13 +0100

    Use @vscode/test-cli
    This CLI means we don’t have to handle certain things. For example, it
    turned out we had a failing test, but the process still exited with
    exit code 0.

--- test.mjs
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import {runTests} from '@vscode/test-electron'
-
-await runTests({
-  extensionDevelopmentPath: new URL('.', import.meta.url).pathname,
-  extensionTestsPath: new URL('test/index.test.js', import.meta.url).pathname
-})
--- test/index.test.js
+++ test/index.test.js
@@ -1,23 +1,25 @@
 const assert = require('node:assert/strict')
 const fs = require('node:fs/promises')
 const path = require('node:path')
-const {test} = require('node:test')
+const {before, afterEach} = require('mocha')
 const {commands, extensions, window, workspace} = require('vscode')
 
-module.exports.run = async () => {
-  const filePath = path.join(__dirname, 'test.md')
+const filePath = path.join(__dirname, 'test.md')
 
+before(async () => {
   const extension = extensions.getExtension('unifiedjs.vscode-remark')
   await extension?.activate()
+})
 
-  await test('use the language server', async () => {
-    await fs.writeFile(filePath, '-   remark\n-   lsp\n-   vscode\n')
-    const document = await workspace.openTextDocument(filePath)
-    await window.showTextDocument(document)
-    await commands.executeCommand('editor.action.formatDocument')
-
-    assert.equal(document.getText(), '* remark\n* lsp\n* vscode\n')
-  })
-
+afterEach(async () => {
   await fs.rm(filePath, {force: true})
-}
+})
+
+test('use the language server', async () => {
+  await fs.writeFile(filePath, '-   remark\n-   lsp\n-   vscode\n')
+  const document = await workspace.openTextDocument(filePath)
+  await window.showTextDocument(document)
+  await commands.executeCommand('editor.action.formatDocument')
+
+  assert.equal(document.getText(), '* remark\n* lsp\n* vscode\n')
+})
